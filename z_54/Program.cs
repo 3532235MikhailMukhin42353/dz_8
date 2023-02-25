9cfabb8878905f11d5c80914c35b0f7fb6a06a00
3532235MikhailMukhin42353 <dzhon.seryy@mail.ru>
2023-02-25 12:46:48 +0300

    sdfsdf :wq

---
+++ z_54/Program.cs
@@ -0,0 +1,53 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива*/
+
+int[,] myArray = new int[4, 4];
+FillArray(myArray);
+PrintArray(myArray);
+Console.WriteLine();
+SortToLower(myArray);
+Console.WriteLine();
+PrintArray(myArray);
+
+
+void FillArray(int[,] myArray)
+{
+    Random random = new Random();
+    for (int i = 0; i < myArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < myArray.GetLength(1); j++)
+        {
+            myArray[i, j] = random.Next(-10, 11);
+        }
+    }
+}
+
+void PrintArray(int[,] myArray)
+{
+    for (int i = 0; i < myArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < myArray.GetLength(1); j++)
+        {
+            Console.Write(myArray[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
